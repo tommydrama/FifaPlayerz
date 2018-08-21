0675987880f127334c9eb6641aa5ea897c767a3d
tommydrama <tomek1darowski@gmail.com>
2018-08-21 12:29:49 +0200

    "First commit"

---
+++ src/main/java/summary/ScoreTable.java
@@ -0,0 +1,21 @@
+package summary;
+
+
+import game.Game;
+import game.Team;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ScoreTable {
+    public static void main(String[] args) {
+
+    }
+}
+//mapa <String team .....>
+
+//metody dodaj
+
+
+
